a6c7dceb48cd02e35a46709faf64ba3937497a11
joaovitorsamora <jvsamora00@gmail.com>
2025-10-09 16:13:09 -0300

    Aplicar migrações do banco ao iniciar a aplicação
    Adicionada lógica no `Program.cs` para aplicar automaticamente
    as migrações pendentes do banco de dados ao iniciar a aplicação.
    Isso foi implementado criando um escopo de serviço para obter
    uma instância do `SistemaDeTarefaDBContext` e chamando o método
    `Migrate()` para garantir que o banco esteja atualizado.

--- GerenciadorDeTarefas/Program.cs
+++ GerenciadorDeTarefas/Program.cs
@@ -37,4 +37,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SistemaDeTarefaDBContext>();
+    db.Database.Migrate();
+}
+
+
 app.Run();
