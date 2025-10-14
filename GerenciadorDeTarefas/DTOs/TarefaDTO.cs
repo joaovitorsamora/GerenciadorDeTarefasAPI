159bbf6226d40a1ac34caf4eb5a9097e03be8e94
joaovitorsamora <jvsamora00@gmail.com>
2025-10-14 09:49:32 -0300

    update

--- GerenciadorDeTarefas/DTOs/TarefaDTO.cs
+++ GerenciadorDeTarefas/DTOs/TarefaDTO.cs
@@ -2,12 +2,20 @@
 {
     public class TarefaDTO
     {
-        public string? Titulo { get; set; }
-        public DateTime DataCriacao { get; set; }
-        public string? ProjetoNome { get; set; }
+        public int? Id { get; set; }
+
+        
+        public int? ProjetoId { get; set; }
+
+        
+        public string? UsuarioNome { get; set; }
+        public string Titulo { get; set; }
+        
         public int UsuarioId { get; set; }
-        public string? StatusTarefa { get; set; }
-        public string? PrioridadeTarefa { get; set; }
-        public List<string>? Tags { get; set; } = new();
+        
+        public string StatusTarefa { get; set; }
+        public string PrioridadeTarefa { get; set; }
+        public string? ProjetoNome { get; set; }
+        public List<string>? Tags { get; set; }
     }
 }
